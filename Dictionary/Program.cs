b71a027861f9209dc02a222d706ac608d27bff04
Son Xuan <son.xuan@mgm-tp.com>
2018-01-29 09:30:27 +0700

    ! init project

---
+++ Dictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+
+namespace Dictionary
+{
+    internal class Program
+    {
+        private static void PrintAppInfo()
+        {
+            var entryAssembly = Assembly.GetEntryAssembly();
+            var titleAttribute = entryAssembly.GetCustomAttribute<AssemblyTitleAttribute>();
+            var versionAttribute = entryAssembly.GetCustomAttribute<AssemblyFileVersionAttribute>();
+            Console.WriteLine($"{titleAttribute.Title} v{versionAttribute.Version}");
+            var copyrightAttribute = entryAssembly.GetCustomAttribute<AssemblyCopyrightAttribute>();
+            Console.WriteLine($"{copyrightAttribute.Copyright}");
+        }
+
+        private static void Main(string[] args)
+        {
+            PrintAppInfo();
+        }
+    }
+}
